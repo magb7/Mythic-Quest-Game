748633f5e24a2e25e72dd0d49561b0b1c4b56520
magb7 <margaux.bedu@gmail.com>
2020-06-14 20:21:08 +0200

    Updates

---
+++ precache-manifest.1060498ccd637a609d317c5b7446031f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8954af4ac323cbb3cf14d578043a0af0",
+    "url": "/Mythic-Quest-Game/index.html"
+  },
+  {
+    "revision": "393d335b2db03cc87129",
+    "url": "/Mythic-Quest-Game/static/css/main.ff9b21bd.chunk.css"
+  },
+  {
+    "revision": "c347268a1211951d976a",
+    "url": "/Mythic-Quest-Game/static/js/2.7852cf13.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Mythic-Quest-Game/static/js/2.7852cf13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393d335b2db03cc87129",
+    "url": "/Mythic-Quest-Game/static/js/main.20029e97.chunk.js"
+  },
+  {
+    "revision": "acbb9bcca1ed2f63215f",
+    "url": "/Mythic-Quest-Game/static/js/runtime-main.ca1bc2d1.js"
+  }
+]);
